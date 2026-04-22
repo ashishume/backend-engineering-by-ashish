47552b335d42058283974534d8ae8c68ed973e55
Ashish Debnath <ashishume@gmail.com>
2026-04-22 14:34:18 +0530

    feat: updates

--- interview-space-backend/app/api/v1/endpoints/user.py
+++ interview-space-backend/app/api/v1/endpoints/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import get_db
 from app.schemas.user import UserCreate, UserResponse
-from app.services.user_service import create_user, get_users
+from app.services.user_service import create_user, fetch_users, get_users
 
 router = APIRouter()
 
@@ -14,4 +14,4 @@ async def create(user: UserCreate, db: AsyncSession = Depends(get_db)) -> UserRe
 
 @router.get("/", response_model=list[UserResponse])
 async def read(db: AsyncSession = Depends(get_db)) -> list[UserResponse]:
-    return await get_users(db)
+    return await fetch_users(db)
--- interview-space-backend/app/services/user_service.py
+++ interview-space-backend/app/services/user_service.py
@@ -1,3 +1,4 @@
+from email import message
 from fastapi import HTTPException, status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -7,19 +8,35 @@ from app.schemas.user import UserCreate
 
 
 async def create_user(db: AsyncSession, user: UserCreate) -> User:
-    new_user = User(**user.model_dump())
-    db.add(new_user)
     try:
-        await db.commit()
-    except IntegrityError as exc:
-        await db.rollback()
+        new_user = User(**user.model_dump())
+        db.add(new_user)
+        try:
+            await db.commit()
+        except IntegrityError as exc:
+            await db.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Email already exists",
+            ) from exc
+        else:
+            await db.refresh(new_user)
+        return new_user
+    except Exception as e:
         raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Email already exists",
-        ) from exc
-    else:
-        await db.refresh(new_user)
-    return new_user
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error processing chat request: {str(e)}",
+        )
+
+
+async def fetch_users(db: AsyncSession) -> list[User]:
+    try:
+        return await get_users(db)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error processing chat request: {str(e)}",
+        )
 
 
 async def get_users(db: AsyncSession) -> list[User]:
